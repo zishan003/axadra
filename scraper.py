fad2692cc0d1919c357b614f6a6bd1c7bcd31ef6
zishan003 <54178511+zishan003@users.noreply.github.com>
2022-06-23 11:13:41 +0530

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,15 @@
+from lib.lib import scraperEngine
+
+def main():
+	scrape = scraperEngine(config)
+	scrape.importIntoDb(scrape.movie_scraper())
+
+if __name__ == '__main__':
+	config= {
+				"host": "127.0.0.1",
+				"user": "root",
+				"database": "test",
+				"password":"",
+				"url": "https://www.themoviedb.org/movie"
+			}
+	main()
